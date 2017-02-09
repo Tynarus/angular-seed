6f6ed43ae73c491b87397c59890e8ede3a3244b2
Mopar <webmaster@runelight.org>
2017-02-09 08:15:47 -0600

    Fixing a few things.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,6 @@
 var webpack = require("webpack");
 var webpackMerge = require("webpack-merge");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyPlugin = require("copy-webpack-plugin");
 var commonConfig = require("./webpack.common.js");
 var helpers = require("./helpers");
 
@@ -30,12 +29,6 @@ module.exports = webpackMerge(commonConfig, {
                 "ENV": JSON.stringify(ENV)
             }
         }),
-        new CopyPlugin([
-            {
-                from: "src/assets",
-                to: "assets"
-            }
-        ]),
         new webpack.LoaderOptionsPlugin({
             options: {
                 htmlLoader: {
